91fbcc61e72e4b7980e98c5f297f1b80d6d5c142
Sergey Dolin <dsame@github.com>
2023-01-20 11:06:40 +0000

    repro

---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require("fs")
+const stream = require('node:stream')
+
+async function uploadChunk(
+  openStream //: () => NodeJS.ReadableStream,
+) { //: Promise<void> {
+  const data = openStream()
+  const ws = new stream.PassThrough()
+  const p = data.pipe(ws)
+  // console.log(p)
+  return p
+}
+
+let done = false
+
+const maxChunkSize = 256
+
+const uploadFile = async (archivePath) => {
+  const fd = fs.openSync(archivePath, "r");
+  const fileSize = fs.statSync(archivePath).size;
+
+  const parallelUploads = [...new Array(4).keys()];
+
+  let offset = 0;
+
+  try {
+    await Promise.all(
+      parallelUploads.map(async () => {
+        while (offset < fileSize) {
+          const chunkSize = Math.min(fileSize - offset, maxChunkSize);
+          const start = offset;
+          const end = offset + chunkSize - 1;
+          offset += maxChunkSize;
+
+          await uploadChunk(
+            () =>
+              fs
+                .createReadStream(archivePath, {
+                  fd,
+                  start,
+                  end,
+                  autoClose: false
+                })
+                .on("error", error => {
+                  throw new Error(
+                    `Cache upload failed because file read failed with ${error.message}`
+                  );
+                }),
+            start,
+            end
+          );
+        }
+      })
+    );
+  } finally {
+    fs.closeSync(fd);
+  }
+  done = true;
+};
+
+const wait = (next) => setTimeout(() => {
+  if (!done) {
+    console.log("wait...")
+    next()
+  }
+}, 1000)
+
+uploadFile(process.argv[1])
+
+wait(wait)
--- poetry-demo/main.py
+++
@@ -1,3 +0,0 @@
-import uuid0
-
-print(uuid0.generate());
